e2742cc16c5a5231a42e07a66e7c881fb99d96ac
Ian Parr <ian.parr@footasylum.com>
2021-01-27 11:00:10 +0000

    Initial commit. Including StringExtensions.TryDeserialize

---
+++ CommonExtensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CommonExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool TryDeserialize<T>(this string value, out T obj)
+        {
+            try
+            {
+                obj = JsonConvert.DeserializeObject<T>(value);
+                return true;
+            }
+            catch (Exception)
+            {
+                obj = default;
+                return false;
+            }
+        }
+    }
+}
